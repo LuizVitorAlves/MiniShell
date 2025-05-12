7e54f96ba20002e9bc4258a18033c2623a6239ac
ubiratan <ubiratan.dev@gmail.com>
2025-05-12 15:03:35 -0300

    começando a tratar a expansao de variavel dentro do echo

---
+++ .history/builtins_20250512112129.c
@@ -0,0 +1,221 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lalves-d <lalves-d@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 15:18:18 by lalves-d          #+#    #+#             */
+/*   Updated: 2025/04/07 15:18:21 by lalves-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+void ft_exit(t_token *tokens, char *input)
+{
+    free(tokens);
+    free(input);
+    exit(0);
+}
+void   ft_echo(t_token *tokens)
+{
+    int check;
+    int i;
+    int j;
+
+    j = 0;
+    i = 0;
+    check = 0;
+    tokens = tokens->next;
+    if(ft_strncmp(tokens->value, "-n",2) == 0)
+    {
+        check =1;
+        tokens = tokens->next;
+    }
+    while(tokens->next)
+    {
+        if(ft_strncmp(tokens->value, "$", 1) == 0)
+        {
+    
+            while(env_vars[i])
+            {
+                ft_strncmp(env_vars[i], tokens->value+1, ft_strlen(tokens->value) == 0);
+                {
+                    while(env_vars[i][j] != '=' || env_vars[i][j] == '=')
+                        j++;
+                    while(env_vars[i][j])
+                        printf("%c", env_vars[i][j++]);
+                }
+                i++;
+            }
+        }
+        else
+            printf("%s ", tokens->value);
+        tokens = tokens->next;
+    }
+    if(check ==0)
+        printf("\n");
+}
+void ft_pwd()
+{
+    char *buffer;
+
+    buffer =  malloc(1024);
+    if(!buffer)
+    {
+        exit(1);
+    }
+    if(getcwd(buffer, 1024)!= NULL)
+    {
+        printf("%s \n", buffer);
+    }
+    else
+    {
+        free(buffer);
+        exit(1);
+    }
+}
+int ft_cd(t_token *tokens, char *path_name)
+{
+    tokens = tokens->next;
+    if (!tokens->value) {
+        printf("cd: missing argument\n");
+        return 1;
+    }
+
+    if (chdir(tokens->value) != 0) {
+        printf("cd: %s error \n", tokens->value);
+        return 1;
+    }
+    ft_strlcpy(path_name+11, tokens->value, (ft_strlen(tokens->value)));
+    ft_strlcpy(path_name+(11 + ft_strlen(tokens->value)), "$", 1);
+
+    return 0;
+}
+
+void create_env_arr(char ***env_vars)
+{
+    int i;
+
+    if (!*env_vars)
+    {
+        *env_vars = malloc(sizeof(char *) * 1025); // aloca array de 1025 ponteiros
+        if (!*env_vars)
+        {
+            perror("malloc failed");
+            exit(EXIT_FAILURE);
+        }
+
+        i = 0;
+        while (i < 1025)
+        {
+            (*env_vars)[i] = NULL; // agora é seguro atribuir
+            i++;
+        }
+    }
+}
+
+
+int max_vars(int num)
+{
+    static int num_vars = -1;
+    if(num == 1)
+        num_vars++;
+    else
+        num_vars--;
+    if(num_vars <= 1024)
+        return (num_vars);
+    return (-1);
+}
+
+
+
+void ft_export(t_token *token)
+{
+    char    *eq_pos;
+    int name_len;
+    char *name;
+    int i;
+    int ret_search;
+    int max_vars_num;
+    char *arg = token->next->value;
+
+    eq_pos = ft_strchr(arg, '=');
+    if (!eq_pos)
+    return ; // ignorar variáveis sem '='
+    name_len = eq_pos - arg;
+    name = malloc(sizeof(char) * (name_len + 1));
+    ft_strlcpy(name, arg, name_len);
+    name[name_len] = '\0';
+    i = 0;
+    ret_search = 0;
+    while (env_vars[i] != NULL)
+    {
+        if (ft_strncmp(env_vars[i], name, name_len) == 0 && env_vars[i][name_len] == '=')
+        {
+            // mesma variável, substituir valor
+            free(env_vars[i]);
+            env_vars[i] = ft_strdup(arg);
+            ret_search = 1;
+            break;
+        }
+        i++;
+    }
+    if (ret_search == 0)
+    {
+        max_vars_num = max_vars(1);
+        if (max_vars_num != -1)
+        env_vars[max_vars_num] = ft_strdup(arg);
+}
+    printf("teste:%s \n", env_vars[0]);
+    free(name);
+}
+
+//remove it after tests
+void ft_env(void)
+{
+    int i = 0;
+    // printf("entrou \n");
+    // printf("env_vars em print_export: %p\n", (void *)env_vars);
+    // printf("declare -x %s\n", env_vars[i]);
+
+    while (env_vars && env_vars[i])
+    {
+        printf("%s\n", env_vars[i]);
+        i++;
+    }
+}
+
+void ft_unset(t_token *token)
+{
+    char *equal_sign;
+    int var_name_len;
+    int i = 0;
+    int j;
+    char *arg=token->next->value;
+
+    if (!arg)
+        return;
+    while (env_vars[i])
+    {
+        equal_sign = ft_strchr(env_vars[i], '=');
+        if (equal_sign)
+        {
+            var_name_len = equal_sign - env_vars[i];
+            if (ft_strlen(arg) == (size_t)var_name_len &&
+                ft_strncmp(env_vars[i], arg, var_name_len) == 0)
+            {
+                max_vars(-1);
+                ft_bzero(env_vars[i], ft_strlen(env_vars[i]));
+                j = i;
+                while (env_vars[j])
+                {
+                    env_vars[j] = env_vars[j + 1];
+                    j++;
+                }
+                continue;
+            }
+        }
+        i++;
+    }
+}
---
+++ .history/main_20250505092945.c
@@ -0,0 +1,107 @@
+#include "minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+char **env_vars = NULL;
+
+void	print_tokens(t_token *tokens)
+{
+	while (tokens)
+	{
+		if (tokens->type == TOKEN_WORD)
+			printf("WORD: '%s'\n", tokens->value);
+		else if (tokens->type == TOKEN_PIPE)
+			printf("PIPE: '%s'\n", tokens->value);
+		else if (tokens->type == TOKEN_REDIR_IN)
+			printf("REDIR_IN: '%s'\n", tokens->value);
+		else if (tokens->type == TOKEN_REDIR_OUT)
+			printf("REDIR_OUT: '%s'\n", tokens->value);
+		else if (tokens->type == TOKEN_APPEND)
+			printf("APPEND: '%s'\n", tokens->value);
+		else if (tokens->type == TOKEN_HEREDOC)
+			printf("HEREDOC: '%s'\n", tokens->value);
+		else if (tokens->type == TOKEN_EOF)
+			printf("EOF\n");
+		tokens = tokens->next;
+	}
+}
+
+void	free_tokens(t_token *tokens)
+{
+	t_token	*tmp;
+
+	while (tokens)
+	{
+		tmp = tokens->next;
+		free(tokens->value);
+		free(tokens);
+		tokens = tmp;
+	}
+}
+
+void	handler_sa_quit(int sig)
+{
+	(void) sig;
+	rl_replace_line("", 0);     // limpa o conteúdo da linha atual
+    write(STDOUT_FILENO, "", 1); // pular linha        // informa que estamos em uma nova linha
+    rl_redisplay();   
+}
+void	handler_ctr_c(int sig)
+{
+	(void) sig;
+
+	rl_replace_line("", 0);     // limpa o conteúdo da linha atual
+    write(STDOUT_FILENO, "\n", 1); // pular linha
+    rl_on_new_line();           // informa que estamos em uma nova linha
+    rl_redisplay();             // reexibe o prompt
+}
+
+int main(int argc, char *argv[])
+{
+    char	*input;
+	char	path_name[1024];
+    t_token    *tokens;
+	struct sigaction sa;
+	struct sigaction sa_quit;
+
+	sa.sa_handler = handler_ctr_c;
+	sa_quit.sa_handler = SIG_IGN;
+	(void )argv;
+    (void )argc;
+	create_env_arr(&env_vars);
+	while(1)
+    {
+		ft_strlcpy(path_name, "minishell$", 11);
+		sigaction(SIGINT, &sa, NULL);
+		sigaction(SIGQUIT, &sa_quit, NULL);
+        input = readline(path_name);
+        if(!input)
+            exit(0);
+        else
+			tokens = tokenize(input);
+        if (input[0])
+			add_history(input);
+        
+        if(ft_strncmp(tokens->value, "exit", 4) == 0 && (tokens->value[4] == ' ' || tokens->value[4] == '\0'))
+            ft_exit(tokens, input);
+		if(ft_strncmp(tokens->value, "echo", 4) == 0)
+			ft_echo(tokens);
+		if(ft_strncmp(tokens->value, "pwd", 3) == 0)
+			ft_pwd();
+		if(ft_strncmp(tokens->value, "cd", 2) == 0)
+			ft_cd(tokens, path_name);
+		if(ft_strncmp(tokens->value, "export", 6) == 0)
+			ft_export(tokens);
+		if(ft_strncmp(tokens->value, "unset", 5) == 0)
+			ft_unset(tokens);
+		if(ft_strncmp(tokens->value, "env", 3) == 0)
+			ft_env();
+		// print_tokens(tokens);
+		free_tokens(tokens);
+        free(input);
+		free(env_vars)
+    }
+}
